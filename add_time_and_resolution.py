5685b5e38664fc990d2a1a4ea8edf42093dca69d
ubuntu <com5620081@gmail.com>
2016-11-14 10:24:11 +0700

    add xml

--- add_time_and_resolution.py
+++ add_time_and_resolution.py
@@ -1,7 +1,11 @@
 import xml.etree.ElementTree as ET
-tree = ET.parse('Movie.xml')
+tree = ET.parse('MovieAll_SPN.xml')
 root = tree.getroot()
 time_tag = ET.Element('time')
 time_tag.text = "100"
 resolution_tag = ET.Element('resolution')
 resolution_tag.text = "HD"
+for movie in root.findall('movie'):
+	movie.append(time_tag)
+	movie.append(resolution_tag)
+tree.write('MovieAll_SPN.xml')
